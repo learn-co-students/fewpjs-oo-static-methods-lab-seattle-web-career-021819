b07be31d73153fbc4955cc7233751388ef187c58
Kimberly Heidorn <heidorkn@gmail.com>
2019-04-08 09:59:29 -0700

    used forEach for titlize

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static titleize(string) {
+    let words = string.split(" ");
+    let sentence = []
+    let exceptWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    words.forEach (word => {
+      if (exceptWords.includes(word)) {
+        sentence.push(word);
+      } else {
+        sentence.push(this.capitalize(word));
+      }
+    })
+    return this.capitalize(sentence.join(' '));
+  }
+
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' );
+  }
 }
