fd4c51eb644d5292dd491be2e5537199b86698cc
github username <github email address>
2019-04-07 20:56:41 -0700

    Done.

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ class Formatter {
     let stringSplit = string.split(" ");
     let newString = "";
     for (let word of stringSplit) {
-      if (!dontCapitalize.includes(word)){
-        newString += (word.charAt(0).toUpperCase() + word.slice(1) + " ");
-      } else if (word === stringSplit[0]){
+      if (!dontCapitalize.includes(word) || word === stringSplit[0]){
         newString += (word.charAt(0).toUpperCase() + word.slice(1) + " ");
       } else {
         newString += (word + " ");
